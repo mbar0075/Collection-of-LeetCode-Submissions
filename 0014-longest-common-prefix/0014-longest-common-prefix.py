103820cc578e6ceeca207821f5157a4780011e66
Matthias Bartolo <103250564+mbar0075@users.noreply.github.com>
2023-05-13 10:35:55 +0200

    Time: 24 ms (36.59%), Space: 13.5 MB (97.83%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+
+        if len(strs) == 1:
+            return strs[0]
+
+        prefix = ""
+        for i in range(len(strs[0])):
+            for j in range(1, len(strs)):
+                if i >= len(strs[j]) or strs[j][i] != strs[0][i]:
+                    return prefix
+            prefix += strs[0][i]
+
+        return prefix
